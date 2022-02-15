33c725b0dded042deefc53271f2e9c3492900b3d
Akshay-Madrikar <akshaymadrikar@gmail.com>
2022-02-15 17:42:29 +0530

    Added stack navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import ShopNavigator from './navigation/ShopNavigator';
 
 import productsReducer from './store/reducers/product';
 
@@ -13,9 +13,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
