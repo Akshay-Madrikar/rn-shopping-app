2cb95063ade581fb83f7229e8c9ed4c31c870a39
Akshay-Madrikar <akshaymadrikar@gmail.com>
2022-02-19 18:48:51 +0530

    styling and navigation completed

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
-import ShopNavigator from './navigation/ShopNavigator';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
+import ShopNavigator from './navigation/ShopNavigator';
 import productsReducer from './store/reducers/product';
+import { useState } from 'react';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -10,7 +13,27 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
